e1d858a2dd8fda40ef2efd20b43107cd675f852f
Samuel3Shin <samuel3.shin@gmail.com>
2023-02-11 22:29:05 -0800

    added logers

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::collections::HashMap;
 use tokio::sync::RwLock;
 use actix_web::{web, App, Error, HttpRequest, HttpResponse, HttpServer, Result};
 use dotenv::dotenv;
+use log::{error, info};
 
 #[macro_use(lazy_static)]
 extern crate lazy_static;
@@ -22,22 +23,18 @@ async fn insert_cache(url: &str, cached_data: CacheItem) {
     CACHE.write().await.insert(url.to_owned(), cached_data);
 }
 
-async fn check_cache(url:&str) -> String{
-    let cache = CACHE.read().await;
-    if let Some(cached_item) = cache.get(url) {
-        return cached_item.result.clone();
-    }
-    "".to_owned()
+async fn check_cache(url:&str) -> Option<String> {
+    CACHE.read().await.get(url).map(|cached_item| {
+        cached_item.result.clone()
+    })
 }
 
 async fn update_cache_timestamp(url:&str) {
-    let mut cache = CACHE.write().await;
-    cache.get_mut(url).unwrap().timestamp = Instant::now();
+    CACHE.write().await.get_mut(url).unwrap().timestamp = Instant::now();
 }
 
 async fn remove_old_cache(cache_lifetime:Duration) {
-    let mut cache = CACHE.write().await;
-    cache.retain(|_, val| val.timestamp.elapsed()<cache_lifetime);
+    CACHE.write().await.retain(|_, val| val.timestamp.elapsed()<cache_lifetime);
 }
 
 async fn handle_request(
@@ -48,9 +45,8 @@ async fn handle_request(
     let url_with_slash = format!("{}", uri);
     let url = &url_with_slash[1..];
 
-    let cached_result = check_cache(url).await;
-    if !cached_result.is_empty() {
-        println!("Cache hit!");
+    if let Some(cached_result) = check_cache(url).await {
+        info!("Cache hit for URL: {}", url);
         update_cache_timestamp(url).await;
         return Ok(HttpResponse::Ok().body(cached_result));
     }
@@ -63,43 +59,59 @@ async fn handle_request(
                     insert_cache(url, cached_data).await;
                     Ok(HttpResponse::Ok().body(body))
                 }
-                Err(error) => Err(actix_web::error::ErrorBadRequest(error))
+                Err(error) => {
+                    error!("Error getting response body for URL {}: {}", url, error);
+                    Err(actix_web::error::ErrorBadRequest("Error getting response body"))
+                }
             }
         }
-        Err(error) => Err(actix_web::error::ErrorBadRequest(error))
+        Err(error) => {
+            error!("Error getting response for URL {}: {}", url, error);
+            Err(actix_web::error::ErrorBadRequest("Error getting response"))
+        }
     }
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     dotenv().ok();
-    let local_host_ip = std::env::var("LOCAL_HOST_IP").expect("LOCAL_HOST_IP must be set.");
-    let local_host_port = std::env::var("LOCAL_HOST_PORT").expect("LOCAL_HOST_PORT must be set.")
-                                .parse::<u16>().unwrap();
+    env_logger::init();
+
+    let local_host_ip = std::env::var("LOCAL_HOST_IP")
+        .expect("LOCAL_HOST_IP must be set.");
+    let local_host_port = std::env::var("LOCAL_HOST_PORT")
+        .expect("LOCAL_HOST_PORT must be set.")
+        .parse()
+        .expect("LOCAL_HOST_PORT must be a valid u16");
+
     let remove_old_cache_interval = std::env::var("REMOVE_OLD_CACHE_INTERVAL")
-                                        .expect("REMOVE_OLD_CACHE_INTERVAL must be set.").parse::<u64>().unwrap();
-    let cache_lifetime = std::env::var("CACHE_LIFETIME").expect("CACHE_LIFETIME must be set.").parse::<u64>().unwrap();
-    
-    let one_second = Duration::new(remove_old_cache_interval, 0);
-    let mut remove_old_cache_interval = tokio::time::interval(one_second);
-    let thirty_seconds = Duration::new(cache_lifetime, 0);
-    
+        .expect("REMOVE_OLD_CACHE_INTERVAL must be set.")
+        .parse()
+        .expect("REMOVE_OLD_CACHE_INTERVAL must be a valid u64");
+    let cache_lifetime = std::env::var("CACHE_LIFETIME")
+        .expect("CACHE_LIFETIME must be set.")
+        .parse()
+        .expect("CACHE_LIFETIME must be a valid u64");
+
+    let remove_old_cache_interval = Duration::from_secs(remove_old_cache_interval);
+    let cache_lifetime = Duration::from_secs(cache_lifetime);
+
     tokio::task::spawn(async move {
+        let mut interval = tokio::time::interval(remove_old_cache_interval);
         loop {
-            remove_old_cache_interval.tick().await;
-            remove_old_cache(thirty_seconds).await;
+            interval.tick().await;
+            remove_old_cache(cache_lifetime).await;
         }
     });
 
     let reqwest_client = reqwest::Client::default();
 
-    HttpServer::new(move || 
-        {
+    HttpServer::new(move || {
         App::new()
-        .app_data(web::Data::new(reqwest_client.clone()))
-        .default_service(web::to(handle_request))
-        })
-        .bind((local_host_ip, local_host_port))?
-        .run()
-        .await
+            .app_data(web::Data::new(reqwest_client.clone()))
+            .default_service(web::to(handle_request))
+    })
+    .bind((local_host_ip, local_host_port))?
+    .run()
+    .await
 }
